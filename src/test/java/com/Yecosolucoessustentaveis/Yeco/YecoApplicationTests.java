aeb4fab392736e65a7b6a5d29a6089d7b1d254c4
Julio Balbino <julio4110@hotmail.com>
2021-09-20 18:11:46 -0300

    Projeto Criado

---
+++ src/test/java/com/Yecosolucoessustentaveis/Yeco/YecoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Yecosolucoessustentaveis.Yeco;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YecoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
